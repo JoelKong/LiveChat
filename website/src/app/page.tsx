38fee39e00be9f55953533165375640af9f53705
Joel Kong <joelkong2001@gmail.com>
2023-12-16 16:47:48 +0800

    feat: added view code

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -5,7 +5,9 @@ import Chat from "@/components/Chat";
 import Particles from "react-particles";
 import type { Engine } from "tsparticles-engine";
 import { loadFirePreset } from "tsparticles-preset-fire";
-import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
   const [chatActive, setChatActive] = useState<boolean>(false);
@@ -21,6 +23,20 @@ export default function Home() {
   return (
     <>
       <Particles options={options} init={loadFire} />
+      <Link
+        className="fixed z-40 w-36 md:w-48 h-12 bottom-4 right-4 rounded-lg bg-gray-600 text-white hover:bg-gray-700 hover:scale-110 transition-all ease-in-out text-md flex flex-row justify-center items-center shadow-lg"
+        href="https://github.com/JoelKong/LiveChat"
+        target="_blank"
+      >
+        <Image
+          className="mr-2"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+          alt="github"
+          width={27}
+          height={27}
+        />
+        View Code
+      </Link>
       <section className="relative w-screen h-screen">
         <Landing setChatActive={setChatActive} />
         {chatActive && <Chat />}
--- website/src/components/Landing.tsx
+++ website/src/components/Landing.tsx
@@ -2,8 +2,10 @@ export default function Landing({ setChatActive }: any): JSX.Element {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-full w-full text-white">
-        <p className="text-7xl font-bold tracking-wide">Live Chat</p>
-        <p className="text-xl tracking-wider mt-4">
+        <p className="text-6xl md:text-7xl font-bold tracking-wide">
+          Live Chat
+        </p>
+        <p className="text-md md:text-xl tracking-wider mt-4 pl-2 pr-2 text-center">
           Experimental web app with websocket, nginx, docker and jenkins
           integration
         </p>
