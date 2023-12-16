bc8bc5835a5b32e4589b84c4992ba2ea2196e0ff
Joel Kong <joelkong2001@gmail.com>
2023-12-16 18:49:26 +0800

    feat: created chat pop up

--- website/src/components/Chat.tsx
+++ website/src/components/Chat.tsx
@@ -1,3 +1,10 @@
 export default function Chat(): JSX.Element {
-  return <div className="text-white">tgdFFFFFFFF</div>;
+  return (
+    <section className="fixed top-28 left-4 md:left-1/4 w-11/12 md:w-1/2 h-3/4 bg-purple-300 rounded-2xl shadow-xl opacity-90 backdrop-brightness-50 flex flex-col items-center">
+      <div className="flex flex-row w-full h-12 mt-4 justify-center items-center border-2">
+        <p className="">Chat</p>
+        <button className="w-12 border-2 justify-items-end">f</button>
+      </div>
+    </section>
+  );
 }
